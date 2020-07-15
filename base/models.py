b8ed807785e3a06d540d3b47bfcf3c38e726ac4c
Dindu5 <chimdindue@gmail.com>
2020-07-15 21:42:38 +0100

    made changes

--- base/models.py
+++ base/models.py
@@ -6,4 +6,4 @@ class Subscriber(models.Model):
     date_created = models.DateTimeField(auto_now_add=True, null = True)
     
     def __str__(self):
-        return self.name
+        return self.email
